1c3024f75f83d20775cae67fc480931db5aee60a
abhinav prajapati <email>
2022-02-02 20:04:04 +0530

    initialize insta

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Post } from "./Post";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app_header">
+        <img className="app_header-image"  src="./instaname.png" alt="" />
+      </div>
+      <h1>ABHINAV</h1>
+      <Post />
+      {/* Header */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default App;
